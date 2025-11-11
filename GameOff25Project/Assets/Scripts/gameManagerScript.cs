b67cdf26a6aadb9bceddc7eb750edf88a1d688b1
Daniel <daniel.a.polen@gmail.com>
2025-11-10 16:54:25 -0800

    script setup

---
+++ GameOff25Project/Assets/Scripts/gameManagerScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+/*
+
+gameManagerScript handles the overall game management tasks.
+|- playerManagerScript manages calling of the player scripts
+|-- playerAbilitiiesScript manages the 3 moves of the player
+|-- playerPowerupsScript manages the function of each powerup
+|-- playerScoreScript will count if the player gains score
+|- busDriverManagerScript manages the bus drivers anger level
+|- pedestrianManagerScript manages the pedestrians movement and animations
+busStopManagerScript manages the level where you buy powerups
+sceneChangerScript manages changing between scenes
+
+*/
+
+public class gameManagerScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GameOff25Project/Assets/Scripts/playerAbilitiies.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class playerAbilitiies : MonoBehaviour
+{
+
+    public void wave()
+    {
+        Debug.Log("Wave Ability Activated");
+    }
+    
+    public void bang()
+    {
+        Debug.Log("Bang Ability Activated");
+    }
+    
+    public void yell()
+    {
+        Debug.Log("Yell Ability Activated");
+    }
+}
